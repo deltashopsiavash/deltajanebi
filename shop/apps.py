41b476254e457e115727825aaf4bb30f619ab301
deltashopsiavash <siavashmamgadery09@gmail.com>
2026-08-25 17:27:29 +0330

    feat: use registered source sites for scraper authorization

--- shop/apps.py
+++ shop/apps.py
@@ -1,4 +1,13 @@
 from django.apps import AppConfig
+
+
 class ShopConfig(AppConfig):
-    default_auto_field="django.db.models.BigAutoField"
-    name="shop"
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "shop"
+
+    def ready(self):
+        from shop.services import source_sync
+        from shop.source_registry import allowed_url, source_brand_terms
+
+        source_sync._allowed_url = allowed_url
+        source_sync._source_brand_terms = source_brand_terms
