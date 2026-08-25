14dbb398945929d01aea896c18f61a03312a2c1a
deltashopsiavash <siavashmamgadery09@gmail.com>
2026-08-25 11:02:48 +0330

    fix: run Django setup only in web service

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
-set -e
-python manage.py makemigrations shop --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+set -eu
+
+if [ "${RUN_DJANGO_SETUP:-0}" = "1" ]; then
+  # Only the web service performs schema/static setup. Workers wait for web health.
+  python manage.py makemigrations shop --noinput
+  python manage.py migrate --noinput
+  python manage.py collectstatic --noinput
+fi
+
 exec "$@"
