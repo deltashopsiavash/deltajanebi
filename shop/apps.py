13d775d3e55f8bc14b0601131ef7a9d0d433e44e
deltashopsiavash <siavashmamgadery09@gmail.com>
2026-08-25 17:33:08 +0330

    feat: use generic category extraction for registered source sites

--- shop/apps.py
+++ shop/apps.py
@@ -7,7 +7,8 @@ class ShopConfig(AppConfig):
 
     def ready(self):
         from shop.services import source_sync
-        from shop.source_registry import allowed_url, source_brand_terms
+        from shop.source_registry import allowed_url, generic_category_names, source_brand_terms
 
         source_sync._allowed_url = allowed_url
         source_sync._source_brand_terms = source_brand_terms
+        source_sync._extract_category_names = generic_category_names
