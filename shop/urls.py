c8c6c9e36fbe5dab67b664181aad904e45264a48
deltashopsiavash <siavashmamgadery09@gmail.com>
2026-08-27 14:26:44 +0330

    Route bot API through Delta parity v14 layer

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from enhancements import account_views as enhanced_accounts
-from enhancements.site_api import bot_api
+from enhancements.site_api_v14 import bot_api
 from . import auth_views, views, wallet_checkout
 
 urlpatterns = [
